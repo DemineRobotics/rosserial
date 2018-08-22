8c1287138bd39e446fac827706ace6c45b48f4fa
Jared Baribeau <jared.baribeau@gmail.com>
2018-08-22 14:34:57 +0700

    Reduced loss of sync problem frequency to roughly every half hour.

--- rosserial_arduino/src/ros_lib/ros.h
+++ rosserial_arduino/src/ros_lib/ros.h
@@ -56,7 +56,7 @@ namespace ros
 
 #elif defined(__AVR_ATmega2560__)
 
-  typedef NodeHandle_<ArduinoHardware, 5, 10, 1024, 1024> NodeHandle;
+  typedef NodeHandle_<ArduinoHardware, 2, 6, 256, 256> NodeHandle;
 
 #elif defined(SPARK)
 
